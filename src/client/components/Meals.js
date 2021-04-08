3cfa4e41aca7c2a9f0833daade72d0f5b65eca89
poorooshka <sepehrinashmil@gmail.com>
2021-04-08 09:57:20 -0700

    I added Meals page of the Meal-Sharing Homework

--- src/client/components/Meals.js
+++ src/client/components/Meals.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function Meals() {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/meals")
+      .then((response) => response.json())
+      .then(setMeals);
+  }, []);
+
   return (
-    <header>
-      <h1> Meals</h1>
-    </header>
+    <div>
+      <header>
+        <h1> Meals </h1>
+      </header>
+
+      <main>
+        {meals.map((meal) => (
+          <div key={meal.id}>
+            <h2>{meal.title}</h2>
+            <p>{meal.description}</p>
+            <Link to={`/meals/${meal.id}`}>Click here for more details</Link>
+          </div>
+        ))}
+      </main>
+    </div>
   );
 }
