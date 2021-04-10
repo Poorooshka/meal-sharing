bd5653d481419f3542609931cc5f528150ac2dfb
poorooshka <sepehrinashmil@gmail.com>
2021-04-10 02:08:24 -0700

    Work in process

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -29,4 +29,39 @@ router.post("/", async (request, response) => {
   }
 });
 
+router.get("/:id", async (request, response) => {
+  try {
+    const mealId = parseInt(request.params.id); // extracting id from what comes after / and converting it to number
+    //console.log(typeof mealId);
+
+    if (isNaN(mealId)) {
+      response.status(400).json("ID should be number");
+      throw error;
+    }
+
+    const meal = await knex("meals").where({ id: mealId });
+    response.send(meal[0]);
+  } catch (error) {
+    throw error;
+  }
+});
+
+router.post("/", async (request, response) => {
+  try {
+    // knex syntax for selecting things. Look up the documentation for knex for further info
+    console.log(request.body);
+    const reservataion = await knex("reservations").insert([
+      {
+        contact_name: request.body.contactName,
+        contact_email: request.body.contactEmail,
+        contact_number: request.body.contactNumber,
+        number_of_guests: request.body.numberOfGuests,
+      },
+    ]);
+    response.json(reservations);
+  } catch (error) {
+    throw error;
+  }
+});
+
 module.exports = router;
